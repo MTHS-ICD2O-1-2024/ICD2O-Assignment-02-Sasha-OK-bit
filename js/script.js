c0f8d1505b22426d363fa382cbd1e3a8f50d730f
Sasha-OK-bit <sasha.vorontsov@stu.ocsb.ca>
2025-03-26 03:18:30 +0000

    PLZ

--- js/script.js
+++ js/script.js
@@ -13,9 +13,10 @@ function calculatebutton() {
   // input
   const Base = parseFloat(document.getElementById("B").value)
   const Height = parseFloat(document.getElementById("H").value);
-  let A = B * H
+  let A = Base * Height
+
   document.getElementById('Area').innerHTML =
-  `A is: ${A.toFixed(2)} <br>
-  ...: ${takeHomeSalary.toFixed(2)}`
+    `A is: ${A.toFixed(2)} <br>
+    ...: ${takeHomeSalary.toFixed(2)}`
 
 }
