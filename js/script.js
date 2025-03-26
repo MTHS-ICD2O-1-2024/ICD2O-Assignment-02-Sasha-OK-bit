def4b38898c305c7aef73b487e17fbdef480a45a
Sasha-OK-bit <sasha.vorontsov@stu.ocsb.ca>
2025-03-26 03:12:04 +0000

    Chnaged for A=bxh

--- js/script.js
+++ js/script.js
@@ -1,12 +1,21 @@
-// Copyright (c) 2020 Mr. Coxall All rights reserved
+
+  // Copyright (c) 2020 Mr. Coxall All rights reserved
 //
 // Created by: Mr. Coxall
 // Created on: Sep 2020
 // This file contains the JS functions for index.html
 
+
 /**
- * Displays an alert with the message "Hello, World!"
- */
-function myButtonClicked() {
-  alert("Hello, World!")
+* Displays an alert with the message "Hello, World!"
+*/
+function calculatebutton() {
+  // input
+  const Base = parseFloat(document.getElementById("b").value)
+  const Height = parseFloat(document.getElementById("H").value);
+  let A = B * H
+  document.getElementById('answer1').innerHTML =
+  `A is: ${A.toFixed(2)} <br>
+  ...: ${takeHomeSalary.toFixed(2)}`
+
 }
